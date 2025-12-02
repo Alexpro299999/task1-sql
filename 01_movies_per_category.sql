8207b8b74186000298cd7793a778bc41e9d4f337
DeepPealer <pratonibratoni@gmail.com>
2025-12-02 17:00:46 +0100

    fix: apply recomendations and add 03_...sql

--- 01_movies_per_category.sql
+++ 01_movies_per_category.sql
@@ -1,7 +1,7 @@
 --Output the number of movies in each category, sorted descending.
-SELECT category.name, COUNT(film_category.film_id) AS film_count
-FROM category 
-LEFT JOIN film_category ON category.category_id = film_category.category_id 
-JOIN film ON film_category.film_id = film.film_id
+SELECT category.name, COUNT(film.film_id) AS film_count
+FROM category
+LEFT JOIN film_category ON category.category_id = film_category.category_id
+LEFT JOIN film ON film_category.film_id = film.film_id
 GROUP BY category.name
-ORDER BY COUNT(film_category.film_id) DESC
+ORDER BY film_count DESC;
--- 02_top_10_actors.sql
+++ 02_top_10_actors.sql
@@ -1,9 +1,16 @@
 --Output the 10 actors whose movies rented the most, sorted in descending order.
-SELECT ac.first_name, ac.last_name, COUNT(ren.rental_id) AS reantal_count
-FROM actor AS ac
-LEFT JOIN film_actor AS fa ON ac.actor_id = fa.actor_id
-JOIN inventory AS inv ON fa.film_id = inv.film_id
-JOIN rental AS ren ON inv.inventory_id = ren.inventory_id
-GROUP BY ac.first_name, ac.last_name
-ORDER BY COUNT(ren.rental_id) DESC
-LIMIT 10
+WITH actor_ranks AS (
+    SELECT
+        actor.first_name,
+        actor.last_name,
+        COUNT(rental.rental_id) AS rental_count,
+        DENSE_RANK() OVER (ORDER BY COUNT(rental.rental_id) DESC) as actor_rank
+    FROM actor
+    JOIN film_actor ON actor.actor_id = film_actor.actor_id
+    JOIN inventory ON film_actor.film_id = inventory.film_id
+    JOIN rental ON inventory.inventory_id = rental.inventory_id
+    GROUP BY actor.actor_id, actor.first_name, actor.last_name
+)
+SELECT first_name, last_name, rental_count
+FROM actor_ranks
+WHERE actor_rank <= 10;
